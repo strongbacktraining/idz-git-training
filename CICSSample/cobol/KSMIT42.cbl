54858b5378315314c438f75679cc89b7adebb1e6
Kenny Smith <Kenny.Smith@strongback.us>
2022-01-28 09:54:24 -0500

    Removed useless comments.
    Signed-off-by: Kenny Smith <Kenny.Smith@strongback.us>

--- CICSSample/cobol/KSMIT42.cbl
+++ CICSSample/cobol/KSMIT42.cbl
@@ -7,23 +7,6 @@
       *                (c) Copyright IBM Corp. 2022                    *
       *                                                                *
       * DISCLAIMER OF WARRANTIES                                       *
-      * You may copy, modify, and distribute these samples, or their   *
-      * modifications, in any form, internally or as part of your      *
-      * application or related documentation. These samples have not   *
-      * been tested under all conditions and are provided to you by    *
-      * IBM without obligation of support of any kind. IBM PROVIDES    *
-      * THESE SAMPLES "AS IS" SUBJECT TO ANY STATUTORY WARRANTIES THAT *
-      * CANNOT BE EXCLUDED. IBM MAKES NO WARRANTIES OR CONDITIONS,     *
-      * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO, THE   *
-      * IMPLIED WARRANTIES OR CONDITIONS OF MERCHANTABILITY, FITNESS   *
-      * FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT REGARDING THESE *
-      * SAMPLES OR TECHNICAL SUPPORT, IF ANY.                          *
-      * You will indemnify IBM or third parties that provide IBM       *
-      * products ("Third Parties") from and against any third party    *
-      * claim arising out of the use, modification or distribution of  *
-      * these samples with your application. You may not use the same  *
-      * path name as the original files/modules. You must not alter or *
-      * delete any copyright information in the Samples.               *
       *                                                                *
       ******************************************************************
 
