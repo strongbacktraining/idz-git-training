e4e965746a147ffc08ea618d200e039ccfa9c1c4
Kenny Smith <Kenny.Smith@strongback.us>
2022-03-10 16:10:01 -0500

    Clean up dirty code. Added warranty.
    Signed-off-by: Kenny Smith <Kenny.Smith@strongback.us>

--- CICSSample/cobol/TIMEZONE.cbl
+++ CICSSample/cobol/TIMEZONE.cbl
@@ -6,7 +6,61 @@
       *                (c) Copyright IBM Corp. 2022
       *
       * DISCLAIMER OF WARRANTIES
-      * Caveat Emptor
+      * Caveat Empt      ******************************************************************
+      *
+      * MODULE NAME    TIMEZONE.CBL
+      *                IBM Developer for z/OS
+      *                (c) Copyright IBM Corp. 2022
+      *
+      * DISCLAIMER OF WARRANTIES
+      *  - For the right price, we'll guarantee that it works.
+      *
+      ******************************************************************
+
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. TIMEZONE.
+
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       77 RAWTIME            PIC S9(15) COMP-3.
+       01  DATE-TEMP         PIC X(15).
+       01  TIME-OUT      PIC X(8).
+       01 WS-MESSAGE  PIC X(8).
+       01 MSG-OUT        PIC X(30).
+       01 CUSTOMER_FNAME  PIC X(30).
+
+       LINKAGE SECTION.
+       PROCEDURE DIVISION.
+      ***************************************************************
+      *    Main section                                             *
+      ***************************************************************
+           MOVE SPACES TO MSG-OUT.
+           EXEC CICS ASKTIME ABSTIME(RAWTIME)
+           END-EXEC.
+
+           EXEC CICS FORMATTIME ABSTIME(RAWTIME)
+                                MMDDYY(DATE-OUT)
+                                DATESEP('-')
+                                TIME(TIME-OUT)
+                                TIMESEP(':')
+           END-EXEC.
+
+           MOVE DATE-OUT TO DATE-TEMP.
+           MOVE TIME-OUT TO TIME-TEMP.
+           MOVE "==>" TO MSG-OUT(1:3)
+           Move DATE-OUT(1:8) to MSG-OUT(6:8).
+           Move " " to MSG-OUT(14:1).
+           Move TIME-OUT(1:8) to MSG-OUT(15:8).
+           EXEC CICS SEND
+                       FROM    (MSG-OUT)
+                       LENGTH (30)
+            END-EXEC.
+
+           EXEC CICS RETURN END-EXEC.
+
+           GOBACK.
+ or
       *
       ******************************************************************
 
